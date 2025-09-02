5a94813ac0f72073517a8af1e7c23e75078787a8
Malthe-kea <mati0002@stud.kea.dk>
2025-09-02 10:39:24 +0200

    init

---
+++ src/main/java/com/example/regionerkommuner/RegionerKommunerApplication.java
@@ -0,0 +1,13 @@
+package com.example.regionerkommuner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionerKommunerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegionerKommunerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/regionerkommuner/RegionerKommunerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.regionerkommuner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegionerKommunerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
