af1fcfceb78ae9ada71229b1c0ead6512a173272
malthe-kea <mati0002@stud.kea.dk>
2025-09-03 14:56:35 +0200

    Fjernet to filer, der ikke blev brugt.

--- src/main/java/com/example/regionerkommuner/service/ApiServiceDeleteKommuner.java
+++
@@ -1,5 +0,0 @@
-package com.example.regionerkommuner.service;
-
-public interface ApiServiceDeleteKommuner {
-    void deleteKommuner();
-}
--- src/main/java/com/example/regionerkommuner/service/ApiServiceDeleteKommunerImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.regionerkommuner.service;
-
-public class ApiServiceDeleteKommunerImpl implements ApiServiceDeleteKommuner {
-    @Override
-    public void deleteKommuner() {
-
-    }
-}
